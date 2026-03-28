0f50c2ce5ef37720385083a33bcd3d0cb28c362b
Keerthi290310 <keerthigujja12@gmail.com>
2026-03-28 11:15:54 +0530

    Add BrowserRouter to index.js
    Wrap App component with BrowserRouter for routing.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // 1. Add this import
 import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* 2. Wrap App with BrowserRouter and add the basename */}
+    <BrowserRouter basename="/UI-Onboarding-multi-step-form-project">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
